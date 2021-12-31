e9d7992a54b1dafb639a5e4a9ee5f588c59103fe
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-12-31 00:36:42 -0300

    Design controller

--- src/Controllers/Design.controller.ts
+++ src/Controllers/Design.controller.ts
@@ -1,57 +1,29 @@
 import express from "express"
-import { pool } from "../Infrastructure/dbPool"
+import Design from "../Repositories/Design.repository"
 
-export const design_getOneItem = async (
+export default async function designController(
   req: express.Request,
   res: express.Response
-) => {
-  let conn
+) {
+  let results: Object[] = []
   try {
-    conn = await pool.getConnection()
-    const designItem = await conn.query(
-      `SELECT * FROM links WHERE Id=${req.params.id} && Category='design'`
-    )
-
-    if (designItem.length === 0) {
-      res.sendStatus(404)
-      return conn.end()
+    const { id } = req.params
+    if (id) {
+      results = await Design.getOneItem(id)
+    } else {
+      results = await Design.getAllItems()
     }
-
-    res.json(designItem)
-    res.sendStatus(200)
-  } catch (err) {
-    return res.sendStatus(400).json({ error: err })
+    if (results.length === 0) {
+      res.status(404).send({ error: "No items found." })
+      return
+    }
+  } catch (error) {
+    res.status(400).send(error)
+    return
   } finally {
-    if (conn) {
-      return conn.end()
+    if (results) {
+      res.send(results)
     }
-  }
-}
-
-export const design_getAllItems = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  let conn
-  try {
-    conn = await pool.getConnection()
-
-    const designItems = await conn.query(
-      `SELECT * FROM links WHERE Category='design'`
-    )
-
-    if (designItems.length === 0) {
-      res.sendStatus(404)
-      return conn.end()
-    }
-
-    res.json(designItems)
-    res.sendStatus(200)
-  } catch (err) {
-    return res.sendStatus(400).json({ error: err })
-  } finally {
-    if (conn) {
-      return conn.end()
-    }
+    return
   }
 }
