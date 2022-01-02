a7b40c4041fe31829c78a15d7c49c6514ef9e56e
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-01 22:46:27 -0300

    mix route - for all items not inside other categories

---
+++ src/Routes/Mix.routes.ts
@@ -0,0 +1,7 @@
+import express from "express"
+import MixController from "../Controllers/Mix.controller"
+
+const router = express.Router()
+router.get("/insight/mix/:id?", MixController)
+
+export default router
--- src/Routes/MixRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { mix_getOneItem, mix_getAllItems } from "../Controllers/Mix.controller"
-
-const router = express.Router()
-router.get("/insight/mix/:id", mix_getOneItem)
-router.get("/insight/mix", mix_getAllItems)
-
-export default router
