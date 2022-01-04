da0af8f57c2afa501d435c0935c4dded5735040a
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-01-04 18:39:12 -0300

    checking running port

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@ import app from "./Configs/app"
 import * as dontenv from "dotenv"
 dontenv.config()
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, () =>
+  console.log("running on port", process.env.PORT)
+)
